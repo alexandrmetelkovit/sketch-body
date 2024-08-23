5d8addcae777199e29a57b73f99342f2503e6e63
alexandrmetelkovit <metel-sanya@mail.ru>
2024-08-23 16:20:59 +0300

    add js sectionAnimation

--- js/burger.js
+++ js/burger.js
@@ -45,29 +45,3 @@ document.addEventListener("click", (e) => {
     closeBurgerLinks();
   }
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  let sections = document.querySelectorAll("section");
-
-  function checkBlocksVisibility() {
-    let windowHeight = window.innerHeight;
-
-    sections.forEach((section) => {
-      let sectionPosition = section.getBoundingClientRect().top;
-
-      if (sectionPosition < windowHeight - 100) {
-        section.style.opacity = "1";
-
-        section.style.transform = "translateY(0)";
-      } else {
-        section.style.opacity = "0";
-
-        section.style.transform = "translateY(50px)";
-      }
-    });
-  }
-
-  checkBlocksVisibility();
-
-  window.addEventListener("scroll", checkBlocksVisibility);
-});
