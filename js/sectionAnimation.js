bf46f633337d6e2aa0abdaa0ee239a5850e3c715
alexandrmetelkovit <metel-sanya@mail.ru>
2024-08-25 18:32:45 +0300

    vendor js css

--- js/sectionAnimation.js
+++ js/sectionAnimation.js
@@ -9,12 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (sectionPosition < windowHeight - 100) {
         section.style.opacity = "1";
-
+        section.style.webkitOpacity = "1"; // Префикс для Safari
+        section.style.mozOpacity = "1"; // Префикс для Firefox
         section.style.transform = "translateY(0)";
+        section.style.webkitTransform = "translateY(0)"; // Префикс для Safari
+        section.style.mozTransform = "translateY(0)"; // Префикс для Firefox
       } else {
         section.style.opacity = "0";
-
-        section.style.transform = "translateY(50px)";
+        section.style.webkitOpacity = "0"; // Префикс для Safari
+        section.style.mozOpacity = "0"; // Префикс для Firefox
+        section.style.transform = "translateY(8px)";
+        section.style.webkitTransform = "translateY(8px)"; // Префикс для Safari
+        section.style.mozTransform = "translateY(8px)"; // Префикс для Firefox
       }
     });
   }
